1c6f3012864582903830f1857432ca7639dc6659
JohnyTheProgrammer <johnyhighskill@gmail.com>
2021-03-09 19:45:28 +0200

    blank

---
+++ venv/Include/Types_and_strings.py
@@ -0,0 +1,35 @@
+fstring = 'Heeeeellllooo Wooorld!!!',
+#for (int i = 0; i < 100; i++)
+'''for i in range(10):'''
+а = 45.6
+b = int(a)
+
+
+fff= 75
+number = input('Please enter number: ')
+flist = []
+dlist = [[index,time,name,price,7,8],
+         [2,9,0,2,3,4],
+         [3,4,5,6,7,8],
+         [4,9,0,2,3,4]]
+z_data = ((dlist[[2],[3]]) + 3 / plus(fff, number))
+for i in dlist:
+    if i > 0 and
+        flist.append(i)
+
+Sqlite3
+ftuple = dlist[:5]
+print(ftuple)
+
+#for i in fstring:
+#for j, item in enumerate(i):
+    #pass
+    #print(j, item)
+
+
+
+f = ()
+selection_tuple = tuple()
+iterable_list = []
+
+
